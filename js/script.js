48ef182e6315fa22e1dd0bf9435b93bcf523f996
samishia27 <samantaroman27@gmail.com>
2022-10-06 18:15:46 -0300

    agregué el javascript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+let header  =
+    <nav class="index">
+        <a href="index.html" class="boton pagina-actual">Inicio</a>
+        <a href="sobre_nosotros.html" class="boton">Nosotros</a>
+        <a href="productos.html" class="boton">Productos</a>
+        <a href="contactanos.html" class="boton">Contactanos</a>
+    </nav>
+document.getElementById(idheader).innerHTML=header
+
+let footer = 
+
+    <h2>Seguinos en nuestras Redes Sociales</h2>
+        <a href="https://www.facebook.com/" target="_blank" class="enlace">
+            <i class="fa-brands fa-square-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/rebeldementedulce/" target="_blank" class="enlace">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+document.getElementById(idfooter).innerHTML=footer
