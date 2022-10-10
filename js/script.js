1794aec70904a663682e9af0c8088ef9a9dcc804
samishia27 <samantaroman27@gmail.com>
2022-10-10 20:01:45 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -10,12 +10,19 @@ let header  =`
 document.getElementById("idheader").innerHTML=header
 
 let footer = `
-    <h2>Seguinos en nuestras Redes Sociales</h2>
+    <div>
+        <h2>Seguinos en nuestras Redes Sociales</h2>
+    </div>
+    <div>
         <a href="https://www.facebook.com/" target="_blank" class="enlace">
             <i class="fa-brands fa-square-facebook"></i>
         </a>
         <a href="https://www.instagram.com/rebeldementedulce/" target="_blank" class="enlace">
             <i class="fa-brands fa-instagram"></i>
-        </a>
+    </a>
+    </div>
+    <div>
+        <p>Derechos Reservados @2022</p>
+    </div>
 `
 document.getElementById("idfooter").innerHTML=footer
